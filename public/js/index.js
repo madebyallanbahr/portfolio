a462c1d0b824bf9b8286a4e7d6bdacd7e87308fa
Made by Allan Bahr <madebyallanbahr@gmail.com>
2024-01-05 18:17:41 -0300

    todo: fix the menu hidden mode

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,14 @@
 // Testing my new keyboard to learn and program a nem ecosystem in Javascript
+
+const toggleMenuIcon = () => {
+  const menuIcon = document.getElementById("menu-toggle");
+  if (window.matchMedia("{max-width: 780px}")) {
+    menuIcon.classList.remove("disabled");
+  } else {
+    menuIcon.classList.add("disabled");
+  }
+};
+
+window.onresize = () => {
+  toggleMenuIcon();
+};
