82345303a96a364fc4ff0c3f82b18f6a52be9f2b
Made by Allan Bahr <madebyallanbahr@gmail.com>
2024-01-11 02:11:59 -0300

    fix: finished structure and styled default from the website

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,1 @@
-// Testing my new keyboard to learn and program a nem ecosystem in Javascript
-
-// const toggleMenuIcon = () => {
-//   const menuIcon = document.getElementById("menu-toggle");
-//   if (window.matchMedia("{max-width: 780px}")) {
-//     menuIcon.classList.remove("disabled");
-//   } else {
-//     menuIcon.classList.add("disabled");
-//   }
-// };
-
-// window.onresize = () => {
-//   toggleMenuIcon();
-// };
+// Added switch theme mecanic
